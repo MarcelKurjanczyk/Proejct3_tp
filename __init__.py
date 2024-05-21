9828c505a329643bd33a33df179ac6fc39f75198
MarcelKurjanczyk <167350429+MarcelKurjanczyk@users.noreply.github.com>
2024-05-21 11:08:08 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from ._core import __doc__, __version__, add, subtract, plot_sine_wave, plot_cosine_wave, plot_square_wave, plot_sawtooth_wave, plot_dft_sinus, plot_dft_cosinus
+
+__all__ = ["__doc__", "__version__", "add", "subtract", "plot_sine_wave", "plot_cosine_wave", "plot_square_wave", "plot_sawtooth_wave", "plot_dft_sinus", "plot_dft_cosinus"]
