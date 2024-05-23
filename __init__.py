e26b301bf4d68b715add5818f43900ea04ce3207
MarcelKurjanczyk <167350429+MarcelKurjanczyk@users.noreply.github.com>
2024-05-23 14:07:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,17 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, add, subtract, plot_sine_wave, plot_cosine_wave, plot_square_wave, plot_sawtooth_wave, plot_dft_sinus, plot_dft_cosinus
+from ._core import __doc__, __version__, add, subtract, plot_sine_wave, plot_cosine_wave, plot_square_wave, plot_sawtooth_wave, plot_dft_sinus, plot_dft_cosinus,signal_show
 
-__all__ = ["__doc__", "__version__", "add", "subtract", "plot_sine_wave", "plot_cosine_wave", "plot_square_wave", "plot_sawtooth_wave", "plot_dft_sinus", "plot_dft_cosinus"]
+__all__ = ["__doc__", "__version__", "add", "subtract", "plot_sine_wave", "plot_cosine_wave", "plot_square_wave", "plot_sawtooth_wave", "plot_dft_sinus", "plot_dft_cosinus","signal_show"]
+
+from scipy.io import wavfile
+import numpy as np
+
+def wizualization(plik_we):
+
+    rate, file = wavfile.read(plik_we)
+    if len(file.shape) > 1:
+        file = file[:, 0]
+    time = np.arange(1000) / rate
+    signal = file[:1000]
+    signal_show(time.tolist(), signal.tolist(), "First 1000 samples")
